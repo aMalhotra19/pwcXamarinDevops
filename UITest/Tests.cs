0707522b627176827ab1333d627495b00cc7e96d
Anju <anjumalhotra1989@gmail.com>
2018-01-16 11:46:39 +0530

    third commit

---
+++ UITest/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void ShouldAddInputsToValidResults()
+        {
+            app.EnterText("txtEntryA", "10");
+            app.EnterText("txtEntryB", "10");
+
+            app.Tap("btnCalculate");
+
+            var outputResult = app.Query("txtOutput").First(result => result.Text == "100");
+
+            Assert.NotNull(outputResult, "Invalid output result");
+        }
+    }
+}
--- XamarinTraining16Jan/XamarinTraining16JanPage.xaml.cs
+++ XamarinTraining16Jan/XamarinTraining16JanPage.xaml.cs
@@ -8,5 +8,12 @@ namespace XamarinTraining16Jan
         {
             InitializeComponent();
         }
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var inputA = int.Parse(this.txtEntryA.Text);
+            var inputB = int.Parse(this.txtEntryB.Text);
+            var result = inputA + inputB;
+            this.txtOutput.Text = result.ToString();
+        }
     }
 }
