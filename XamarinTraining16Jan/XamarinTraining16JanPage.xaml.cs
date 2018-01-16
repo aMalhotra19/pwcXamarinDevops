4ffee1f2b2401b096ca1283a3085b2d366905d98
Anju <anjumalhotra1989@gmail.com>
2018-01-16 11:17:21 +0530

    Sec commit

---
+++ XamarinTraining16Jan/XamarinTraining16JanPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinTraining16Jan
+{
+    public partial class XamarinTraining16JanPage : ContentPage
+    {
+        public XamarinTraining16JanPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
